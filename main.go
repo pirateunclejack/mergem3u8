4a0243f2f70c9f53e8c71f894f3f96d576d6cb33
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-04-28 00:51:31 +0800

    Update log, use goroutine

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"io"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sort"
 	"strconv"
+	"sync"
 
 	"github.com/spf13/viper"
 )
@@ -17,10 +22,17 @@ type config struct {
 
 var C config
 
+var (
+	Trace 	*log.Logger
+	Info 	*log.Logger
+	Warning *log.Logger
+	Error 	*log.Logger
+)
+
 func loadConfig() {
 	ex, err := os.Executable()
 	if err != nil {
-		log.Fatalf("Can not get Executable file path, with error: %v", err)
+		Error.Printf("Can not get Executable file path, with error: %v", err)
 	}
 	exPath := filepath.Dir(ex)
 
@@ -29,7 +41,7 @@ func loadConfig() {
 	viper.SetConfigType("yaml")
 	err = viper.ReadInConfig()
 	if err != nil {
-		log.Fatalf("Fatal error config file: %v \n", err)
+		Error.Printf("Fatal error config file: %v \n", err)
 	}
 
 	C.path = viper.GetStringSlice("path")
@@ -37,71 +49,101 @@ func loadConfig() {
 }
 
 func mergeTs(dir, output string) {
-	log.Printf("Merging TS files under: %v", dir)
-
-	for i := 0; ; i++ {
-		srcFilePath := filepath.Join(dir, strconv.Itoa(i))
-
-		_, err := os.Stat(srcFilePath)
-		if os.IsNotExist(err) {
-			log.Printf("TS file %v not exist.", srcFilePath)
-			break
-		}
-
+	Info.Printf("Merging TS files under: %v", dir)
+	if _, err := os.Stat(output); err == nil {
+		Info.Printf("Output file already exists: %s", output);
+		return
+	} else {
+		inputs_files, err := ioutil.ReadDir(dir)
 		if err != nil {
-			log.Fatalf("Check file %v state error: %v\n", srcFilePath, err)
+			Error.Printf("Failed to read source directory:%s with error: %v",dir, err)
 		}
-
-		src, err := ioutil.ReadFile(srcFilePath)
+		regex, err := regexp.Compile("[0-9]+")
 		if err != nil {
-			log.Printf("Can not read TS file %v with error: %v", srcFilePath, err)
-			break
+			Error.Printf("Could not compile regexp: %v", err)
 		}
-
-		log.Printf("Merging TS file: %s", srcFilePath)
-
-		dst, err := os.OpenFile(output, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-		log.Println(dst.Name())
-		if err != nil {
-
-			log.Fatalf("Error open output file, with error: %v", err)
+		var input_file_names []int
+		for _, i := range inputs_files {
+			if err != nil {
+				Error.Printf("Failed to convert string to regex: %v", err)
+			}
+			if regex.MatchString(i.Name()) {
+				// Info.Printf("%s Match regex",i.Name())
+				file_name_int, err := strconv.Atoi(i.Name())
+				if err != nil {
+					Error.Printf("Failed to convert string to int: %v", err)
+				}
+				input_file_names = append(input_file_names, file_name_int)
+			} else {
+				Info.Printf("%s does not match regex.", i.Name())
+			}
 		}
-		if _, err := dst.Write(src); err != nil {
-
-			log.Fatalf("Error write to output file, with error: %v", err)
+		sort.Ints(input_file_names)
+		Info.Printf("input_file_names: %v", input_file_names)
+		for _, input_file_name := range input_file_names {
+			file_name_string := strconv.Itoa(input_file_name)
+			srcFilePath := filepath.Join(dir, file_name_string)
+			_, err := os.Stat(srcFilePath)
+			if os.IsNotExist(err) {
+				Info.Printf("TS file %v not exist.", srcFilePath)
+				return
+			}
+			if err != nil {
+				Error.Printf("Check file %v state error: %v\n", srcFilePath, err)
+			}
+			src, err := ioutil.ReadFile(srcFilePath)
+			if err != nil {
+				Info.Printf("Can not read TS file %v with error: %v", srcFilePath, err)
+				return
+			}
+			Info.Printf("Merging TS file: %s", srcFilePath)
+			dst, err := os.OpenFile(output, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+			log.Println(dst.Name())
+			if err != nil {
+				Error.Printf("Error open output file, with error: %v", err)
+			}
+			if _, err := dst.Write(src); err != nil {
+				Error.Printf("Error write to output file, with error: %v", err)
+			}
+			if err := dst.Close(); err != nil {
+				Error.Printf("Error close output file, with error : %v", err)
+			}
 		}
-		if err := dst.Close(); err != nil {
+	}
+}
 
-			log.Fatalf("Error close output file, with error : %v", err)
-		}
-
+func init() {
+	loadConfig()
+	file, err := os.OpenFile(C.log, os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
+	Trace   = log.New(io.MultiWriter(file, os.Stdout), "TRACE:   ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info    = log.New(io.MultiWriter(file, os.Stdout), "INFO:    ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(io.MultiWriter(file, os.Stdout), "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error   = log.New(io.MultiWriter(file, os.Stderr), "ERROR:   ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func main() {
-
-	loadConfig()
-	logTo, err := os.OpenFile(C.log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-
-	if err != nil {
-		log.Fatalf("Error opening logfile: %v", err)
-	}
-
-	defer logTo.Close()
-
-	log.SetOutput(logTo)
-
+	var wg sync.WaitGroup
 	for _, basePath := range C.path {
 		files, err := ioutil.ReadDir(basePath)
 		if err != nil {
-			log.Fatalf("Error opening dir: %v", err)
+			Error.Printf("Error opening dir: %v", err)
 		}
 
 		for _, f := range files {
 			if f.IsDir() {
-				mergeTs(filepath.Join(basePath, f.Name()), filepath.Join(basePath, f.Name()+".mp4"))
+				wg.Add(1)
+				go func(basePath string, f fs.FileInfo) {
+					defer wg.Done()
+					src_dir := filepath.Join(basePath, f.Name())
+					dest_file := filepath.Join(basePath, f.Name()+".mp4")
+					mergeTs(src_dir, dest_file)
+				}(basePath, f)
 			}
 		}
 	}
+	wg.Wait()
 }
